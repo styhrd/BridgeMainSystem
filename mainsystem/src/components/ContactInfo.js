9f236565a16dc65935d73a894fc1790a00467ca3
styhrd <juanmiguelcaparas76@gmail.com>
2024-07-20 18:32:38 +0800

    cONTACRT

--- mainsystem/src/components/ContactInfo.js
+++ mainsystem/src/components/ContactInfo.js
@@ -4,7 +4,20 @@ const ContactInfo = () => {
     return (
         <div>
             <h1>Contact Info</h1>
-            <p>This is the Contact Info section.</p>
+            <ul>
+                <li>
+                    <strong>Phone:</strong> <a href="tel:+639989925241">+63 998 992 5241</a>
+                </li>
+                <li>
+                    <strong>Email:</strong> <a href="mailto:hello@bridge360.ph">hello@bridge360.ph</a>
+                </li>
+                <li>
+                    <strong>Email:</strong> <a href="mailto:iambridge360@gmail.com">iambridge360@gmail.com</a>
+                </li>
+                <li>
+                    <strong>Website:</strong> <a href="https://www.bridge360.ph" target="_blank" rel="noopener noreferrer">www.bridge360.ph</a>
+                </li>
+            </ul>
         </div>
     );
 }
